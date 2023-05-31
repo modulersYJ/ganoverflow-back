6680495d549369d21aa29b1afea7a571d1d8ad19
horz.d <he1236@ajou.ac.kr>
2023-05-31 14:39:43 +0900

    init db connection & user (login, register) dto and the others.. 2

--- nest/src/app.service.ts
+++ nest/src/app.service.ts
@@ -1,13 +1,12 @@
-/* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
   }
 
   getBye(): string {
-    return 'Bye World!';
+    return "Bye World!";
   }
 }
