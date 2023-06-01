f0bdda1b8bc050ad6094002fd960116caa470283
hongregii <hongregii@hongregiiui-Macmini.local>
2023-06-02 01:34:35 +0900

    res 생성

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from "./auth/auth.module";
 import { APP_GUARD } from "@nestjs/core";
 import { AuthGuard } from "./auth/auth.guard";
 import { ChatbotModule } from "./chatbot/chatbot.module";
+import { ConversationsModule } from './conversations/conversations.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { ChatbotModule } from "./chatbot/chatbot.module";
     UserModule,
     AuthModule,
     ChatbotModule,
+    ConversationsModule,
   ],
 
   controllers: [AppController],
