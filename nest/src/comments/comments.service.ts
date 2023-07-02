9e60d24f3df60a48a6841ebdb869b19b4987a9a7
horiz.d <65459616+ABizCho@users.noreply.github.com>
2023-07-03 06:16:16 +0900

    Junk: 간단한 타이포 수정 + 엔티티 상 user id가 number로 잘못 작성되어 있던 부분 수정 및 추가 필드 명시했습니다.

--- nest/src/comments/comments.service.ts
+++ nest/src/comments/comments.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Injectable } from "@nestjs/common";
+import { CreateCommentDto } from "./dto/create-comment.dto";
+import { UpdateCommentDto } from "./dto/update-comment.dto";
 
 @Injectable()
 export class CommentsService {
   create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+    return "This action adds a new comment";
   }
 
   findAll() {
